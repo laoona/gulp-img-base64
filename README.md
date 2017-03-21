897862a16444beb66d6076e904abe864d49e5b39
laoono <laoono@163.com>
2017-03-21 17:25:15 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
 # gulp-img-base64
+
+[![NPM version][npm-image]][npm-url]
+[![Build Status][travis-image]][travis-url]
+[![Coverage Status][coveralls-image]][coveralls-url]
+
+A simple gulp plugin, used to transform image into base64 string.
+
+## Install
+```
+npm install gulp-img-base64
+```
+
+## Usage
+```javascript
+var tobase64 = require("gulp-img-base64");
+tobase64(options);
+```
+
+### Example
+```javascript
+var tobase64 = require("gulp-img-base64");
+gulp.task('build-html' , function(){
+  return gulp.src("./html-init/**/*.html")
+      .pipe(tobase64({
+          maxsize:0.5,        
+          ignore:'image_loading.png',
+          pathrep: {
+              reg:/\/public\/bizapp\d*\//g ,
+              rep:'./public/'
+          }
+      }))
+      .pipe(gulp.dest("./html"))
+});
+```
+
+## API
+
+### maxsize
+The max size of images for compiling, the default value is 1 and the unit is 'KB'
+
+### ignore
+Ignore files, it can be string, array or regular. <br>
+```
+'image' , ['image','abc'] or /\/abc\//g
+```
+
+### pathrep
+Transform file path into readable path like this :
+```
+/public/bizapp1007/image.png ==> ./public/image.png
+```
+
+## Running test
+```
+make test
+```
+
+## License
+MIT
+
+[npm-url]: https://npmjs.org/package/gulp-img-base64
+[npm-image]: http://img.shields.io/npm/v/gulp-img-base64.svg?style=flat-square
+[travis-url]: https://travis-ci.org/whxaxes/gulp-img-base64
+[travis-image]: http://img.shields.io/travis/whxaxes/gulp-img-base64.svg?style=flat-square
+[coveralls-url]: https://coveralls.io/r/whxaxes/gulp-img-base64
+[coveralls-image]: https://img.shields.io/coveralls/whxaxes/gulp-img-base64.svg?style=flat-square
---
+++ index.js
@@ -0,0 +1,95 @@
+/**
+ * @author: laoono
+ * @date:  2017-03-20
+ * @time: 21:40
+ * @contact: laoono.com
+ * @description: #
+ */
+
+'use strict';
+var through = require('through2');
+var path = require('path');
+var fs = require('fs');
+var URL_REG = /("|'|\()((?:\/?(?:[\w.-]|\.|\.\.)*\/)*?[\w-]*?\.(png|jpeg|jpg|gif|bmp))(?:\?[\w=]*)?("|'|\))/g;
+
+/**
+ * @param opt
+ *  - log     {Boolean}  是否输出log, 默认值为1
+ *  - maxsize {Number}   小于该值的图片将会被编译为base64，单位为kb
+ *  - ignore  {RegExp|Array|String} 用于过滤部分不想编译的图片名
+ *  - pathrep {Object}   将路由地址的图片路径替换成相对地址,格式为{reg:'',rep:''}
+ *      - reg   {RegExp|String}  用于匹配替换的正则
+ *      - rep   {String}         替换字符
+ */
+module.exports = function (opt) {
+    opt = opt || {};
+    var ignore = opt.ignore;
+    var maxsize = +opt.maxsize || 1;
+    var pathrep = opt.pathrep || {};
+
+    // 匹配包含?base64的资源文件
+    var matchReg = /(\?base64)/gi;
+
+    //将ignore参数转成正则
+    var type = Object.prototype.toString.call(ignore);
+    var igReg;
+    if (type === '[object Array]') {
+        igReg = new RegExp(formatReg(ignore.join('|')), 'g');
+    } else if (type === '[object String]') {
+        igReg = new RegExp(formatReg(ignore), 'g');
+    } else if (type === '[object RegExp]') {
+        igReg = ignore;
+    } else {
+        igReg = null;
+    }
+
+    /**
+     * 流的中转方法
+     * @param file
+     * @param encoding
+     * @param done
+     * @private
+     */
+    var _transform = function (file, encoding, done) {
+        var str = String(file.contents);
+        var p = file.path.substring(0, file.path.lastIndexOf(path.sep));
+
+        str = str.replace(URL_REG, function (m) {
+            var prefix = RegExp.$1;       //$1为匹配的开头的'或'或(
+            var suffix = RegExp.$4;       //$4为匹配的结尾的'或'或)
+            var imgkind = RegExp.$3;      //$3为匹配的图片类型
+            var imgPath = RegExp.$2;      //$2为路径内容
+
+            //判断ignore的值并进行相应处理
+            if ((igReg && m.match(igReg)) || !matchReg.test(m)) return m;
+
+            //如果是路由地址，则替换为相对地址，否则根据文件位置匹配出相应的绝对地址
+            if (pathrep && (pathrep.reg instanceof RegExp) && m.match(pathrep.reg)) {
+                imgPath = imgPath.replace(pathrep.reg, pathrep.rep || '');
+            } else {
+                imgPath = path.resolve(p, imgPath);
+            }
+
+            //如果图片文件小于1kb则替换为base64格式
+            if (fs.existsSync(imgPath) && (fs.statSync(imgPath).size / 1024) < maxsize) {
+                var source = fs.readFileSync(imgPath).toString('base64');
+                var imageSource = 'data:image/' + imgkind + ';base64,' + source;
+                return prefix + imageSource + suffix;
+            }
+
+            return m;
+        });
+
+        file.contents = new Buffer(str);
+
+        return done(null, file);
+    };
+
+    function formatReg(str) {
+        return str.replace(/\\|\.|\+/g, function (m) {
+            return '\\' + m
+        });
+    }
+
+    return through.obj(_transform)
+};
